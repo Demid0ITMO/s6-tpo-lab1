646466b446e66de69da639613ffda12b94173d9e
Demid0 <demid04ka@mail.ru>
2026-03-13 18:18:09 +0300

    feat: task3 (+2/8 classes)

--- src/main/java/task3/Main.java
+++ src/main/java/task3/Main.java
@@ -3,9 +3,12 @@ package task3;
 import task3.item.CornFlakes;
 import task3.item.DentrassiUnderwear;
 import task3.item.Item;
+import task3.item.MSEReport;
 import task3.item.ScvorshneineMatrass;
 import task3.person.Arthur;
 import task3.person.Ford;
+import task3.person.MigrationServiceEmployee;
+import task3.person.Person;
 
 public class Main {
   public static void main(String[] args) {
@@ -28,10 +31,50 @@ public class Main {
       new DentrassiUnderwear(),
       new CornFlakes(),
       new ScvorshneineMatrass(),
+      new CornFlakes(),
+      new CornFlakes(),
+      new CornFlakes(),
     };
 
     for (Item i : items) {
       System.out.println("когда " + a.lookAt(i) + ", уверенность: " + a.getConfidence());
     }
+
+    System.out.println();
+
+    MigrationServiceEmployee jack = new MigrationServiceEmployee("Jack", "Москва");
+    Person[] persons = {
+      f,
+      new Person("Виталя", "Москва"),
+      a,
+      new Person("Чингачкук", "Марс"),
+      new Person("Сер гей", "Моа"),
+      new Person("Серега", "Москва"),
+    };
+
+    for (Person p: persons) {
+      String s = p.getName() + " путешествует с " + p.getCurrentPlanet();
+      p.travelTo(jack.getHomePlanet());
+      s += " на " + p.getCurrentPlanet();
+      System.out.println(s);
+    }
+
+    System.out.println("когда " + a.lookAt(jack) + ", уверенность: " + a.getConfidence());
+
+    for (Person possibleMigrant: persons) {
+      MSEReport rep = new MSEReport();
+      boolean checkRes = jack.checkPerson(possibleMigrant, rep);
+      
+      System.out.println(rep.getText());
+      if (!checkRes) {
+        System.out.println(possibleMigrant.getName() + (rep.willDeparted() ? "" : " не") + " будет депортирован");
+      }
+
+      if (possibleMigrant instanceof Arthur) {
+        System.out.println("когда " + ((Arthur) possibleMigrant).lookAt(rep) + ", уверенность: " + a.getConfidence());
+      }
+    }
+
+
   }
 }
